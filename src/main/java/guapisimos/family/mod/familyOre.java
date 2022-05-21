eac12efc402c07f7c8c7efd534749c143a9fc1af
dph000 <72009170+dph000@users.noreply.github.com>
2022-05-21 19:47:43 +0200

    family
    family

---
+++ src/main/java/guapisimos/family/mod/familyOre.java
@@ -0,0 +1,34 @@
+package guapisimos.family.mod;
+
+import java.util.Random;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+
+public class familyOre extends Block {
+    public familyOre() {
+        super(FabricBlockSettings.of(Material.STONE).strength(2, 2.0f));
+    }
+    
+    protected int getExperienceWhenMined(Random random) {
+        return MathHelper.nextInt(random, 0, 2);
+    }
+
+    public void onStacksDropped(BlockState state, ServerWorld world, BlockPos pos, ItemStack stack) {
+        super.onStacksDropped(state, world, pos, stack);
+        if (EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, stack) == 0) {
+            int i = this.getExperienceWhenMined(world.random);
+            if (i > 0) {
+                this.dropExperience(world, pos, i);
+            }
+        }
+    }
+}
---
+++ src/main/java/guapisimos/family/mod/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package guapisimos.family.mod.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import guapisimos.family.mod.Family;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		
+	}
+}
